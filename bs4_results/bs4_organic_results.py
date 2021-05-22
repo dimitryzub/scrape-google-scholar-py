39c8aaf8143420866ac03da7965b1d2a4a988f44
Dimitry Zub <78694043+dimitryzub@users.noreply.github.com>
2021-05-22 09:40:20 +0300

    Update bs4_organic_results.py
    (refactor): changed variable names, order of JSON keys, added a try, except block.

--- bs4_results/bs4_organic_results.py
+++ bs4_results/bs4_organic_results.py
@@ -26,21 +26,24 @@ def bs4_srape_organic_results():
   # Container where all needed data located
   for result in soup.select('.gs_ri'):
     title = result.select_one('.gs_rt').text
+    title_link = result.select_one('.gs_rt a')['href']
     publication_info = result.select_one('.gs_a').text
     snippet = result.select_one('.gs_rs').text
     cited_by = result.select_one('#gs_res_ccl_mid .gs_nph+ a')['href']
     related_articles = result.select_one('a:nth-child(4)')['href']
-    all_article_versions = result.select_one('a~ a+ .gs_nph')['href']
-    link = result.select_one('.gs_rt a')['href']
+    try:
+      all_article_versions = result.select_one('a~ a+ .gs_nph')['href']
+    except:
+      all_article_versions = None
 
     data.append({
       'title': title,
+      'title_link': title_link,
       'publication_info': publication_info,
       'snippet': snippet,
       'cited_by': f'https://scholar.google.com{cited_by}',
       'related_articles': f'https://scholar.google.com{related_articles}',
       'all_article_versions': f'https://scholar.google.com{all_article_versions}',
-      'link': link,
     })
 
   print(json.dumps(data, indent = 2, ensure_ascii = False))
