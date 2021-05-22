d6cfd921596afcda916104325ba5e82980a9c6b3
Dimitry Zub <78694043+dimitryzub@users.noreply.github.com>
2021-05-22 09:26:53 +0300

    Create serpapi_author_co_author_results.py

---
+++ serpapi_results/serpapi_author_co_author_results.py
@@ -0,0 +1,19 @@
+from serpapi import GoogleSearch
+import os
+
+def serpapi_srape_author_co_author_results():
+  params = {
+    "api_key": os.getenv("API_KEY"),
+    "engine": "google_scholar_author",
+    "author_id": "m8dFEawAAAAJ",
+    "hl": "en",
+  }
+
+  search = GoogleSearch(params)
+  results = search.get_dict()
+
+  for authors in results['co_authors']:
+    author_name = authors['name']
+    author_affiliations = authors['affiliations']
+    author_link = authors['link']
+    print(f'{author_name}\n{author_affiliations}\n{author_link}\n')
