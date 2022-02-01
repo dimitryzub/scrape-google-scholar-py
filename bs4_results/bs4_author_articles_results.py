bfb5364bf02c1ec263fd5cd05d057fc7c1e6982e
Dimitry Zub☀️ <78694043+dimitryzub@users.noreply.github.com>
2022-02-01 09:26:36 +0200

    update: remove proxy

--- bs4_results/bs4_author_articles_results.py
+++ bs4_results/bs4_author_articles_results.py
@@ -6,12 +6,8 @@ headers = {
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
 }
 
-proxies = {
-  'http': os.getenv('HTTP_PROXY')
-}
-
 def bs4_srape_author_articles_result():
-  html = requests.get('https://scholar.google.com/citations?hl=en&user=m8dFEawAAAAJ', headers=headers, proxies=proxies).text
+  html = requests.get('https://scholar.google.com/citations?hl=en&user=m8dFEawAAAAJ', headers=headers).text
   soup = BeautifulSoup(html, 'lxml')
 
   for article_info in soup.select('#gsc_a_b .gsc_a_t'):
