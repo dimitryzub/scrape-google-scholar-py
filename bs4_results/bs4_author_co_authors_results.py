052620d410f1b08728da0ca66221b429fb3f553e
Dmitiry Zub☀️ <78694043+dimitryzub@users.noreply.github.com>
2022-05-27 12:38:03 +0300

    Update bs4_author_co_authors_results.py

--- bs4_results/bs4_author_co_authors_results.py
+++ bs4_results/bs4_author_co_authors_results.py
@@ -3,11 +3,11 @@ import requests, lxml
 
 headers = {
     'User-agent':
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
 }
 
 
-def bs4_srape_author_co_authors_result():
+def bs4_scrape_author_co_authors_result():
   html = requests.get('https://scholar.google.com/citations?hl=en&user=m8dFEawAAAAJ', headers=headers).text
 
   soup = BeautifulSoup(html, 'lxml')
@@ -17,3 +17,6 @@ def bs4_srape_author_co_authors_result():
     author_affiliations = container.select_one('.gsc_rsb_a_ext').text
     author_link = container.select_one('#gsc_rsb_co a')['href']
     print(f'{author_name}\n{author_affiliations}\nhttps://scholar.google.com{author_link}\n')
+    
+    
+bs4_scrape_author_co_authors_result()
