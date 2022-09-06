feb72c41a0d4783eb98091e8c2487f41f0212be8
Dmitiry Zub☀️ <78694043+dimitryzub@users.noreply.github.com>
2022-09-06 03:42:33 -0700

    feat: add pagination to serpapi scholar organic results

--- serpapi_results/serpapi_organic_results.py
+++ serpapi_results/serpapi_organic_results.py
@@ -1,30 +1,83 @@
 from serpapi import GoogleSearch
 import os, json
 
-def serpapi_srape_organic_results():
-  params = {
-    "api_key": os.getenv("API_KEY"),
-    "engine": "google_scholar",
-    "q": "samsung",
-  }
+def serpapi_scrape_google_scholar_organic_results():
+    params = {
+        # https://docs.python.org/3/library/os.html
+        'api_key': os.getenv('API_KEY'), # serpapi api key
+        'engine': 'google_scholar',      # serpapi parsing engine
+        'q': 'blizzard',                 # search query
+        'hl': 'en',                      # language
+        'start': 0                       # first page
+    }
 
-  search = GoogleSearch(params)
-  results = search.get_dict()
+    search = GoogleSearch(params)        # where data extracts on the backend
 
-  # It looks a bit akward but the point is that you can grab everything you need in 2-3 lines of code as below.
-  for result in results['organic_results']:
-    print(f"Title: {result['title']}\nPublication info: {result['publication_info']['summary']}\nSnippet: {result['snippet']}\nCited by: {result['inline_links']['cited_by']['link']}\nRelated Versions: {result['inline_links']['related_pages_link']}\n")
+    organic_results_data = []
 
-  # Or if you want more readable code, here's one example.
-  data = []
+    while True:
+        results = search.get_dict()       # JSON -> Python dict
 
-  for result in results['organic_results']:
-    data.append({
-      'title': result['title'],
-      'publication_info': result['publication_info']['summary'],
-      'snippet': result['snippet'],
-      'cited_by': result['inline_links']['cited_by']['link'],
-      'related_versions': result['inline_links']['related_pages_link'],
-    })
+        print(f'Currently extracting page #{results["serpapi_pagination"]["current"]}..')
 
-  print(json.dumps(data, indent = 2, ensure_ascii = False))
+        for result in results['organic_results']:
+            position = result['position']
+            title = result['title']
+            publication_info_summary = result['publication_info']['summary']
+            result_id = result['result_id']
+            link = result.get('link')
+            result_type = result.get('type')
+            snippet = result.get('snippet')
+  
+            try:
+              file_title = result['resources'][0]['title']
+            except: file_title = None
+  
+            try:
+              file_link = result['resources'][0]['link']
+            except: file_link = None
+  
+            try:
+              file_format = result['resources'][0]['file_format']
+            except: file_format = None
+  
+            try:
+              cited_by_count = int(result['inline_links']['cited_by']['total'])
+            except: cited_by_count = None
+  
+            cited_by_id = result.get('inline_links', {}).get('cited_by', {}).get('cites_id', {})
+            cited_by_link = result.get('inline_links', {}).get('cited_by', {}).get('link', {})
+  
+            try:
+              total_versions = int(result['inline_links']['versions']['total'])
+            except: total_versions = None
+  
+            all_versions_link = result.get('inline_links', {}).get('versions', {}).get('link', {})
+            all_versions_id = result.get('inline_links', {}).get('versions', {}).get('cluster_id', {})
+  
+            organic_results_data.append({
+              'page_number': results['serpapi_pagination']['current'],
+              'position': position + 1,
+              'result_type': result_type,
+              'title': title,
+              'link': link,
+              'result_id': result_id,
+              'publication_info_summary': publication_info_summary,
+              'snippet': snippet,
+              'cited_by_count': cited_by_count,
+              'cited_by_link': cited_by_link,
+              'cited_by_id': cited_by_id,
+              'total_versions': total_versions,
+              'all_versions_link': all_versions_link,
+              'all_versions_id': all_versions_id,
+              'file_format': file_format,
+              'file_title': file_title,
+              'file_link': file_link,
+            })
+
+        if 'next' in results.get('serpapi_pagination', {}):
+            search.params_dict.update(dict(parse_qsl(urlsplit(results['serpapi_pagination']['next']).query)))
+        else:
+            break
+
+    return organic_results_data
