a8ba3ff3f44438ba6b33f3ace1ab1f64b6d62710
Dmitiry Zub☀️ <78694043+dimitryzub@users.noreply.github.com>
2022-09-06 03:36:03 -0700

    feat: add pagination to serpapi profile results

--- serpapi_results/serpapi_profile_results.py
+++ serpapi_results/serpapi_profile_results.py
@@ -1,26 +1,51 @@
 from serpapi import GoogleSearch
-import os
+from urllib.parse import urlsplit, parse_qsl
+import os, json
 
-def serpapi_srape_profile_results():
+
+def serpapi_scrape_all_authors():
     params = {
-        "api_key": os.getenv("API_KEY"),
-        "engine": "google_scholar_profiles",
-        "hl": "en",
-        "mauthors": "samsung"
+        'api_key': os.getenv('API_KEY'),      # SerpApi API key
+        'engine': 'google_scholar_profiles',  # profile results search engine
+        'mauthors': 'blizzard',               # search query
     }
-
     search = GoogleSearch(params)
-    results = search.get_dict()
 
-    for result in results['profiles']:
-      name = result['name']
-      try:
-        email = result['email']
-      except:
-        email = None
-      affiliation = result['affiliations']
-      cited_by = result['cited_by']
-      interests = result['interests'][0]['title']
-      interests_link = result['interests'][0]['link']
+    profile_results_data = []
 
-      print(f'{name}\n{email}\n{affiliation}\n{cited_by}\n{interests}\n{interests_link}\n')
+    profiles_is_present = True
+    while profiles_is_present:
+
+        profile_results = search.get_dict()
+
+        for profile in profile_results['profiles']:
+
+            print(f'Currently extracting {profile["name"]} with {profile["author_id"]} ID.')
+
+            thumbnail = profile['thumbnail']
+            name = profile['name']
+            link = profile['link']
+            author_id = profile['author_id']
+            affiliations = profile['affiliations']
+            email = profile.get('email')
+            cited_by = profile.get('cited_by')
+            interests = profile.get('interests')
+
+            profile_results_data.append({
+                'thumbnail': thumbnail,
+                'name': name,
+                'link': link,
+                'author_id': author_id,
+                'email': email,
+                'affiliations': affiliations,
+                'cited_by': cited_by,
+                'interests': interests
+            })
+
+            if 'next' in profile_results['pagination']:
+                # split URL in parts as a dict() and update search 'params' variable to a new page
+                search.params_dict.update(dict(parse_qsl(urlsplit(profile_results['pagination']['next']).query)))
+            else:
+                profiles_is_present = False
+
+    return profile_results_data
