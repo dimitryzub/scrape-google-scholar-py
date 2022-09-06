9389a3fddba0a0d33d2e960148ef8adc4a0a9d49
Dmitiry Zub☀️ <78694043+dimitryzub@users.noreply.github.com>
2022-09-06 03:43:54 -0700

    refactor: extract result ID and citation data

--- serpapi_results/serpapi_cite_results.py
+++ serpapi_results/serpapi_cite_results.py
@@ -1,15 +1,41 @@
 from serpapi import GoogleSearch
 import os
 
-def serpapi_srape_cite_results():
-  params = {
-    "api_key": os.getenv("API_KEY"),
-    "engine": "google_scholar_cite",
-    "q": "FDc6HiktlqEJ"
-  }
+def organic_results():
+    params = {
+        "api_key": os.getenv("API_KEY"),      # serpapi api key
+        "engine": "google_scholar",           # serpapi parsing engine
+        "q": "blizzard",                      # search query
+        "hl": "en"                            # language
+    }
 
-  search = GoogleSearch(params)
-  results = search.get_dict()
+    search = GoogleScholarSearch(params)
+    results = search.get_dict()
 
-  for cite in results['citations']:
-    print(f'Title: {cite["title"]}\nSnippet: {cite["snippet"]}\n')
+    return [result["result_id"] for result in results["organic_results"]]
+
+
+def cite_results():
+
+    citations = []
+
+    for citation_id in organic_results():
+        params = {
+            "api_key": os.getenv("API_KEY"),
+            "engine": "google_scholar_cite",
+            "q": citation_id
+        }
+
+        search = GoogleScholarSearch(params)
+        results = search.get_dict()
+
+        for result in results["citations"]:
+            institution = result["title"]
+            citation = result["snippet"]
+
+            citations.append({
+                "institution": institution,
+                "citations": citation
+            })
+
+    return citations
