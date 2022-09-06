4af219c7fe26f3e46a92bff76c9a990da3bf9bef
Dmitiry Zub☀️ <78694043+dimitryzub@users.noreply.github.com>
2022-09-06 03:23:21 -0700

    refactor: update co-authors script name, add JSON output, minor improvements

--- custom_solution/bs4_author_co_authors_results.py
+++ custom_solution/bs4_author_co_authors_results.py
@@ -1,22 +1,30 @@
-from bs4 import BeautifulSoup
-import requests, lxml
-
-headers = {
-    'User-agent':
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36'
-}
+from parsel import Selector
+import requests, os, json, re
 
 
-def bs4_scrape_author_co_authors_result():
-  html = requests.get('https://scholar.google.com/citations?hl=en&user=m8dFEawAAAAJ', headers=headers).text
+def parsel_scrape_author_co_authors():
+    params = {
+        'user': '_xwYD2sAAAAJ',       # user-id
+        'hl': 'en'                    # language
+    }
 
-  soup = BeautifulSoup(html, 'lxml')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    }
 
-  for container in soup.select('.gsc_rsb_aa'):
-    author_name = container.select_one('#gsc_rsb_co a').text
-    author_affiliations = container.select_one('.gsc_rsb_a_ext').text
-    author_link = container.select_one('#gsc_rsb_co a')['href']
-    print(f'{author_name}\n{author_affiliations}\nhttps://scholar.google.com{author_link}\n')
-    
-    
-bs4_scrape_author_co_authors_result()
+    co_authors = []
+
+    html = requests.get('https://scholar.google.com/citations', params=params, headers=headers, timeout=30)
+    selector = Selector(text=html.text)
+
+    for result in selector.css('.gsc_rsb_aa'):
+        co_authors.append({
+            'name': result.css('.gsc_rsb_a_desc a::text').get(),
+            'title': result.css('.gsc_rsb_a_ext::text').get(),
+            'link': f"https://scholar.google.com{result.css('.gsc_rsb_a_desc a::attr(href)').get()}",
+            'email': result.css('.gsc_rsb_a_ext.gsc_rsb_a_ext2::text').get(),
+            # https://regex101.com/r/awJNhL/1
+            'thumbnail': f"https://scholar.googleusercontent.com/citations?view_op=view_photo&user={re.search(r'user=(.*)&', result.css('.gsc_rsb_a_desc a::attr(href)').get()).group(1)}"
+        })
+
+    print(json.dumps(co_authors, indent=2, ensure_ascii=False))
