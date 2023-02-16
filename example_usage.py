398d835770de3117578c00749bed5047fea5d04e
Dmitiry Zub☀️ <dimitryzub@gmail.com>
2023-02-16 07:41:11 +0000

    update example usage file

--- example_usage.py
+++ example_usage.py
@@ -1,58 +1,103 @@
-# custom backend
-from custom_backend.google_scholar_profiles_results import scrape_google_scholar_profiles
-from custom_backend.google_scholar_author_info_all_articles import scrape_google_scholar_author_data
-from custom_backend.google_scholar_organic_search import scrape_google_scholar_organic_results
-from custom_backend.google_scholar_top_mandates_metrics import google_scholar_top_mandates_metrics
-from custom_backend.google_scholar_top_publications_metrics import google_scholar_top_publication_metrics
+from google_scholar_py.custom_backend.author_info_all_articles import CustomGoogleScholarAuthor
+from google_scholar_py.custom_backend.organic_search import CustomGoogleScholarOrganic
+from google_scholar_py.custom_backend.profiles_results import CustomGoogleScholarProfiles
+from google_scholar_py.custom_backend.top_mandates_metrics import CustomGoogleScholarTopMandates
+from google_scholar_py.custom_backend.top_publications_metrics import CustomGoogleScholarTopPublications
 
-# serpapi backend
-from serpapi_backend.serpapi_google_scholar_organic_results import serpapi_scrape_google_scholar_organic_results
-from serpapi_backend.serpapi_google_scholar_organic_cite_results import serpapi_scrape_google_scholar_cite_results
-from serpapi_backend.serpapi_google_scholar_profile_results import serpapi_scrape_google_scholar_profile_results
-from serpapi_backend.serpapi_google_scholar_author_results import serpapi_scrape_google_scholar_author_results
+from google_scholar_py.serpapi_backend.author_results import SerpApiGoogleScholarAuthor
+from google_scholar_py.serpapi_backend.profile_results import SerpApiGoogleScholarProfiles
+from google_scholar_py.serpapi_backend.organic_results import SerpApiGoogleScholarOrganic
+from google_scholar_py.serpapi_backend.organic_cite_results import SerpApiGoogleScholarOrganicCite
+
+import json
+
+parser = CustomGoogleScholarAuthor()
+data = parser.scrape_google_scholar_author_data(
+    user_id='nHhtvqkAAAAJ',
+    operating_system='win',
+    parse_articles=True,
+    article_pagination=True
+)
+print(json.dumps(data, indent=2))
+
+parser = CustomGoogleScholarOrganic()
+data = parser.scrape_google_scholar_organic_results(
+    query='minecraft ss ws',
+    operating_system='win',
+    pagination=False,
+    save_to_csv=True
+)
+print(json.dumps(data, indent=2))
+
+parser = CustomGoogleScholarProfiles()
+data = parser.scrape_google_scholar_profiles(
+    query='blizzard',
+    operating_system='win',
+    pagination=False,
+    save_to_csv=False,
+    save_to_json=False
+)
+print(json.dumps(data, indent=2))
 
 
-'''
-Custom backend
+parser = CustomGoogleScholarTopMandates()
+data = parser.scrape_top_mandates_metrics(
+    operating_system='win',
+    save_to_csv=True,
+    save_to_json=False
+)
+print(json.dumps(data, indent=2))
 
-Google Scholar search operators could also be used:
-    label:computer_vision "Michigan State University"|"U.Michigan"
 
-This will search all profiles from 2 universities based on computer vision query
-'''
-# data = scrape_google_scholar_profiles(query='label:computer_vision "Michigan State University"|"U.Michigan"', pagination=False, operating_system='win')
-# data = scrape_google_scholar_author_data(user_id='I1wOjTYUyYAC', operating_system='win') # or 'windows'
-# data = google_scholar_top_mandates_metrics(operating_system='win', save_to_csv=True)
-data = google_scholar_top_publication_metrics(operating_system='win', save_to_csv=True)
-print(data)
+parser = CustomGoogleScholarTopPublications()
+data = parser.scrape_top_publication_metrics(
+    operating_system='win',
+    category='bus',
+    save_to_csv=True,
+    save_to_json=False
+)
+print(json.dumps(data, indent=2))
 
-# data = scrape_google_scholar_organic_results(query='blizzard effects xanax', pagination=False, operating_system='win', save_to_json=True)
+# ------------------------SerpApi backend------------------------------
 
-# for profile in data:
-#     print(profile['name'])
-#     print(profile['interests'])
+author_parser = SerpApiGoogleScholarAuthor()
+data = author_parser.scrape_google_scholar_author_results(
+    author_id='nHhtvqkAAAAJ',
+    api_key='serpapi_api_key',
+    parse_articles=False,
+    article_pagination=False
+)
+print(json.dumps(data, indent=2))
 
-# print(json.dumps(data, indent=2))
 
-'''
-SerpApi backend
+profile_parser = SerpApiGoogleScholarProfiles()
+data = profile_parser.scrape_google_scholar_profile_results(
+    query='blizzard',
+    api_key='serpapi_api_key',
+    pagination=False,
+)
+print(json.dumps(data, indent=2))
 
-Same as in the custom backend, Google Scholar search operators could be used also.
-'''
 
-data = serpapi_scrape_google_scholar_cite_results(
-        query='minecraft',
-        api_key='...',
-        pagination=False,
-        lang='en'
-    )
-    
-# extracting bottom links
+organic_parser = SerpApiGoogleScholarOrganic()
+data = organic_parser.scrape_google_scholar_organic_results(
+    query='minecraft ss ws', 
+    api_key='serpapi_api_key', 
+    pagination=True
+)
+print(json.dumps(data, indent=2))
+
 for result in data:
-    for citations in result['links']: 
-        print(citations['name']) # or ['link']
+    print(result['title'])
 
-# extracting citations
+cite_parser = SerpApiGoogleScholarOrganicCite()
+data = cite_parser.scrape_google_scholar_cite_results(
+    query='minecraft ss ws', 
+    api_key='serpapi_api_key', 
+    pagination=True
+)
+print(json.dumps(data, indent=2))
+
 for result in data:
     for citations in result['citations']: 
-        print(citations['title']) # or ['snippet']      
+        print(citations['title']) # or ['snippet'] 
